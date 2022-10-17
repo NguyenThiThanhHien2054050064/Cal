787182340d6fa997a3165ee4ae7ac96c301b47da
NguyenThiThanhHien2054050064 <2054050064hien@ou.edu.vn>
2022-10-17 11:17:34 +0700

    Xu ly cong

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -102,6 +102,7 @@ namespace calculator
             this.button1.TabIndex = 3;
             this.button1.Text = "cộng";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
